c83771873b5db8ef1b2ad198b8d7340c19dbda9c
Patricia <patriciaparreirasainz@gmail.com>
2025-05-13 11:55:21 +0200

    project structure

---
+++ src/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// AUDIOS
+const winSound = new Audio('/audio/win.mp3');
+const gameOverSound = new Audio('audio/game-over.mp3');
+const popSound = new Audio('audio/pop.mp3');
+
+// HTML ELEMENTS
+
+
+// NEW ELEMENTS
+
+
+// FUNCTIONS
+
+
+// EVENTS
